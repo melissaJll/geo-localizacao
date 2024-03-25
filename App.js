4de37d0582bb0f268312eb72591317797f5feb51
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-25 08:48:47 -0300

    MapView

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+    // Definição do zoom, Quanto menor mais próximo do mapa
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView style={estilos.mapa} />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const estilos = StyleSheet.create({
+  container: { flex: 1 },
+  mapa: { width: "100%", height: "100%" },
 });
