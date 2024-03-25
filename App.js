70ee597ea7ea01cf5b436db840c95c4004e3a6bf
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-25 09:44:58 -0300

    marker funcionando e exibindo dados da localização

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
+import { useState } from "react";
 import { Image, StatusBar, StyleSheet, View } from "react-native";
 
-// Importação da biblioteca de mapa e o sub-componente "Marker"
 import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [localizacao, setLocalizacao] = useState({
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 0.8,
+    longitudeDelta: 0.8,
+  });
   const regiaoInicialMapa = {
-    // Brasil
-
     latitude: -23.5489,
     longitude: -46.6388,
 
     latitudeDelta: 0.8,
     longitudeDelta: 0.8,
   };
-
-  // Marker
-  const localizacao = {
-    latitude: -33.867886,
-    longitude: -63.987,
-    latitudeDelta: 0.8,
-    longitudeDelta: 0.8,
+  const marcarLocal = (event) => {
+    // console.log(event.nativeEvent);
+    setLocalizacao({
+      ...localizacao,
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
   };
 
   return (
@@ -27,13 +31,12 @@ export default function App() {
       <StatusBar />
       <View style={estilos.container}>
         <MapView
+          onPress={marcarLocal}
           style={estilos.mapa}
           initialRegion={regiaoInicialMapa}
           mapType="hybrid"
         >
-          <Marker coordinate={localizacao} draggable>
-            <Image source={require("./assets/ghost.png")} />
-          </Marker>
+          <Marker coordinate={localizacao} draggable />
         </MapView>
       </View>
     </>
